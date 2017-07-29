60b2a9dbda0056bc6ea543cbea195ae39236250c
Chandan Kumar <scorpio.ckumar@gmail.com>
2017-07-29 19:39:46 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,1 +1,45 @@
 /*Update following in your WordPress theme's functions.php file */
+
+// Remove Query String from Static Resources
+function remove_cssjs_ver( $src ) {
+if( strpos( $src, '?ver=' ) )
+ $src = remove_query_arg( 'ver', $src );
+return $src;
+}
+add_filter( 'style_loader_src', 'remove_cssjs_ver', 10, 2 );
+add_filter( 'script_loader_src', 'remove_cssjs_ver', 10, 2 );
+
+// Remove Emojis
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
+// Remove Shortlink
+remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+
+// Disable Embed
+function disable_embed(){
+wp_dequeue_script( 'wp-embed' );
+}
+add_action( 'wp_footer', 'disable_embed' );
+
+// Disable XML-RPC
+add_filter('xmlrpc_enabled', '__return_false');
+
+// Remove RSD Link
+remove_action( 'wp_head', 'rsd_link' ) ;
+
+// Hide Version
+remove_action( 'wp_head', 'wp_generator' ) ;
+
+// Remove WLManifest Link
+remove_action( 'wp_head', 'wlwmanifest_link' ) ;
+
+// Disable JQuery Migrate
+function deregister_qjuery() {  
+    if ( !is_admin() ) {
+        wp_deregister_script('jquery');
+    }
+}  
+add_action('wp_enqueue_scripts', 'deregister_qjuery'); 
